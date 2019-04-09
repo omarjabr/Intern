3ebcaaf5356c3c76e61b8c5d340dd83e5811da14
Omar Jabr <omarjabr165@gmail.com>
2019-04-09 11:57:20 +0200

    Initial commit

---
+++ areeba/src/Footer.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, TextInput, Alert, Button, View } from 'react-native';
+
+const Footer = () => {
+        return(
+            <View style={styles.container}>
+                <Text style={styles.removeButton}>
+                    Remove completed items
+                </Text>
+            </View>
+        );
+    
+}
+export default Footer;
+
+const styles = StyleSheet.create({
+    container:{
+        // flex:1,
+        borderTopWidth: 1,
+        borderTopColor: '#D3D3D3',
+        height:30,
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        marginBottom: 36
+    },
+    removeButton:{
+        position: 'absolute',
+        bottom:0
+    }
+});
---
+++ areeba/src/List.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { StyleSheet, ScrollView, Text, TextInput, View, TouchableOpacity, CheckBox } from 'react-native';
+import { CheckBox } from 'react-native-elements';
+
+const List = () => {
+    state = {
+        Name:[
+            {
+                id:1,
+                title:'Check Email'  
+            },
+            {
+                id:2,
+                title:'Take a shower'
+            },
+            {
+                id:3,
+                title:'Brush teeth'
+            },
+            {
+                id:4,
+                title:'eat breakfast'
+            }
+        ]
+    }
+    return(
+        <View style={styles.container}>
+        <TextInput 
+        style={styles.textInput}
+        underlineColorAndroid="#D3D3D3"
+        placeholder="Enter an item!"
+        />
+        <ScrollView>
+            {
+                this.state.Name.map((item) => (
+                    <TouchableOpacity
+                    key={item.id}>
+                            <CheckBox
+                            title="name"/>
+                    </TouchableOpacity>
+                ))
+            }
+        </ScrollView>
+        </View>
+    );
+}
+export default List;
+const styles = StyleSheet.create({
+    container:{
+      flex:1,
+      flexDirection: 'column',
+    },
+    textInput:{
+      height:50,
+      fontSize: 15,
+    }
+  });
